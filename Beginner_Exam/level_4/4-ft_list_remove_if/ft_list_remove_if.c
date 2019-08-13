7aa1e64b7ec89a990aaa6c044ade69d7837b9d14
Evan Johnston <me@evandjohnston.com>
2019-08-12 21:02:43 -0700

    Cleaned up syntax, fixed end-of-list behavior

--- Beginner_Exam/level_4/4-ft_list_remove_if/ft_list_remove_if.c
+++ Beginner_Exam/level_4/4-ft_list_remove_if/ft_list_remove_if.c
@@ -5,18 +5,19 @@
 
 void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-	t_list *head = *begin_list;
-
-	if (head == NULL)
+	if (begin_list == NULL || *begin_list == NULL)
 		return;
 
-	if (cmp(head->data, data_ref) == 0)
+	t_list *cur = *begin_list;
+
+	if (cmp(cur->data, data_ref) == 0)
 	{
-		*begin_list = (*(begin_list))->next;
-		free(head);
+		*begin_list = cur->next;
+		free(cur);
 		ft_list_remove_if(begin_list, data_ref, cmp);
 	}
-	ft_list_remove_if(&(*(begin_list))->next, data_ref, cmp);
+	cur = *begin_list;
+	ft_list_remove_if(&cur->next, data_ref, cmp);
 }
 
 //---------------------------------------------------------------------
